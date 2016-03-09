58da5f5856862a4ab3da2fc936e7a7f839b7d0e0
Jose Vildosola <ext_jovildosola@Falabella.cl>
2016-03-09 18:03:16 -0300

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = 'FETCH_POSTS';
+
+const API_KEY = '?key19844';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+}
